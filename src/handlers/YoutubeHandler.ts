46bb68e271d3fba8a54cb5210d8e415539db929d
Simon Galet <simon.galet@gmail.com>
2018-04-21 19:15:00 +0200

    Put back logs

--- src/handlers/YoutubeHandler.ts
+++ src/handlers/YoutubeHandler.ts
@@ -18,12 +18,14 @@ export default class YoutubeHandler {
     }
 
     public async play(message: Message, url: string): Promise<void> {
+        console.log("play");
         if (!this._isInGoodVoiceChannel(message)) {
             return;
         }
 
         if (!this._voiceConnection) {
             await this._joinVoiceChannel(message);
+            console.log("_joinVoiceChannel finished");
         }
 
         this._playOrQueueContent(message, url);
@@ -107,6 +109,7 @@ export default class YoutubeHandler {
     }
 
     private _isInGoodVoiceChannel(message: Message): boolean {
+        console.log("_isInGoodVoiceChannel");
         const { voiceChannel } = message.member;
 
         if (!voiceChannel) {
@@ -121,14 +124,18 @@ export default class YoutubeHandler {
     }
 
     private async _joinVoiceChannel(message: Message): Promise<void> {
+        console.log("_joinVoiceChannel");
         const { voiceChannel } = message.member;
 
+        console.log("Before voiceChannel.join()");
         const voiceConnection = await voiceChannel.join();
+        console.log("After voiceChannel.join()");
         this._voiceChannel = voiceChannel;
         this._voiceConnection = voiceConnection;
     }
 
     private _playOrQueueContent(message: Message, url: string): void {
+        console.log("_playOrQueueContent", url);
         if (!ytdl.validateLink(url)) {
             message.reply("Elle mène à rien ton URL.");
             return;
@@ -142,6 +149,7 @@ export default class YoutubeHandler {
     }
 
     private _playContent(message: Message, url: string): void {
+        console.log("_playContent", url);
         if (!this._voiceConnection) {
             return;
         }
@@ -158,6 +166,7 @@ export default class YoutubeHandler {
     }
 
     private _contentEnded(): void {
+        console.log("_contentEnded");
         this._currentUrl = undefined;
         if (this._queue.length) {
             const youtubeUrl = this._queue.shift();
